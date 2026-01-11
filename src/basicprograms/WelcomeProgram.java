a149935a1c04be34da1eae439f7261b572c35c87
purviagarawal224-cse <purviagarwal224@gmail.com>
2026-01-11 23:01:07 +0530

    [Purvi Agrawal] Add.Added Welcome program file

---
+++ src/basicprograms/WelcomeProgram.java
@@ -0,0 +1,11 @@
+package basicprograms;
+
+public class WelcomeProgram {
+    public static void displaymessage(){
+        System.out.println("Welcome to Bridgelabz!");
+    }
+    public static void main(String[] args){
+        displaymessage();
+    }
+
+}
