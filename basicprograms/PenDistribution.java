cf751d0592e617fb55249a9887aaecda094d3259
purviagarawal224-cse <purviagarwal224@gmail.com>
2026-01-14 20:33:58 +0530

    [Purvi Agrawal] Add.Added Pens distribution program file

---
+++ basicprograms/PenDistribution.java
@@ -0,0 +1,9 @@
+public class PenDistribution {
+    public static void main(String[] args){
+        int numberOfPen = 14;
+        int penPerStudent = 14/3;
+        int remainingPen = 14%3;
+
+        System.out.println("The pen per student is " + penPerStudent + "And the remaining pen not distributed "+ remainingPen);
+    }
+}
