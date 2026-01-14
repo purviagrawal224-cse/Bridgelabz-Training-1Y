c722b580c06c0b11a450dcd92df7ad5ecc892b32
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-01-14 21:35:28 +0530

    Delete basicprograms/KiloMetersToMeters.java

--- basicprograms/KiloMetersToMeters.java
+++
@@ -1,8 +0,0 @@
-public class KiloMetersToMeters {
-    public static void main(String[] args){
-        double distanceInKilometres = 10.8;
-        double distanceInMiles = (10.8 *1.6);
-
-        System.out.println("The distance " + distanceInKilometres + "km in miles is " + distanceInMiles);
-    }
-}
