bec7a771debd26ef51178cd8f9157ab4a5c6d542
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-01-14 21:52:46 +0530

    Delete basicprograms/Avg_PCM_Marks.java

--- basicprograms/Avg_PCM_Marks.java
+++
@@ -1,9 +0,0 @@
-public class Avg_PCM_Marks {
-    public static void main(String[] args){
-        int math = 94, physics = 95, chemistry = 96;
-        int total = 94+95+96;
-        double percentage = (total*100)/300;
-
-        System.out.println("Sam's average in PCM is " + percentage);
-    }
-}
