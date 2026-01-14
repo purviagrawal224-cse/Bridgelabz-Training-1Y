167a5231c7ef0913a722e3e126efd1e51b47d938
purviagarawal224-cse <purviagarwal224@gmail.com>
2026-01-14 20:37:18 +0530

    [Purvi Agrawal] Add.Added Pens Average PCM marks program file

---
+++ basicprograms/Avg_Marks_PCM.java
@@ -0,0 +1,9 @@
+public class Avg_Marks_PCM {
+    public static void main(String[] args){
+        int math = 94, physics = 95, chemistry = 96;
+        int total = 94+95+96;
+        double percentage = (total*100)/300;
+
+        System.out.println("Sam's average in PCM is " + percentage);
+    }
+}
