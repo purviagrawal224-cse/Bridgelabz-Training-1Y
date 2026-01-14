5944b8c1b902b9c13f983648d19f04659ce7d9af
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-01-14 20:18:27 +0530

    Delete Practice_problem/Level_1 directory

--- Practice_problem/Level_1/SamMarks.java
+++
@@ -1,9 +0,0 @@
-package Level_1;
-
-public class SamMarks {
-    public static void main(String[] args) {
-        double P=95, C=96, M=94;
-        double avg=((P+C+M)/300)*100;
-        System.out.println("Sam’s average mark in PCM is: "+avg+"%");
-    }
-}
