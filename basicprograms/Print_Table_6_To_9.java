e8036c7468a1b7690cd8991278e3ee76781fa222
Purvi Agrawal <purviagrawal224@gmail.com>
2026-02-01 22:17:59 +0530

    Assignment 4 --> Level 1 --> Problem 5

---
+++ basicprograms/Print_Table_6_To_9.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Print_Table_6_To_9 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+        int[] table = new int[4];
+        int idx = 0;
+        for(int i=6; i<=9; i++){
+            table[idx]=n*i;
+            idx++;
+        }
+        idx = 0;
+        for(int i = 6; i<=9; i++){
+            System.out.println(n+" x "+i+" = "+table[idx]);
+            idx++;
+        }
+        sc.close();
+    }
+}
