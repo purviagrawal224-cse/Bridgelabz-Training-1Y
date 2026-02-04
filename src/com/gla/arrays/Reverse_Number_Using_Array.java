3e273031495ff2f1c82c2082e792954cde82d020
Purvi Agrawal <purviagrawal224@gmail.com>
2026-02-04 19:59:49 +0530

    [Purvi Agrawal].Fix commit message

---
+++ src/com/gla/arrays/Reverse_Number_Using_Array.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Reverse_Number_Using_Array {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+        int num = Math.abs(n);
+        if(num==0){
+            System.out.println("Reverse number is 0");
+            sc.close();
+            return;
+        }
+        int[] digits = new int[10];
+        int idx = 0;
+        while(num!=0 && idx<10){
+            digits[idx] = num%10;
+            num /= 10;
+            idx++;
+        }
+        long rev = 0;
+        for(int i=0; i<idx; i++) rev = rev*10+digits[i];
+        if(n<0) rev = -rev;
+        System.out.println("Reversed number is "+rev);
+        sc.close();
+    }
+}
---
+++ src/com/gla/arrays/Table.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number : ");
+        int n = sc.nextInt();
+        int[] table = new int[10];
+        for(int i=1; i<=10; i++) table[i-1] = n*i;
+        for(int i = 1; i<=10; i++) System.out.println(n+" x "+i+" = "+table[i-1]);
+        sc.close();
+    }
+}
