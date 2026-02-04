d6421a86b1df7672fed598d9d5633363136fca62
Purvi Agrawal <purviagrawal224@gmail.com>
2026-02-04 21:46:44 +0530

    Purvi Agrawal] Add. PCM_Marks_Grade_By_Using_2D_Array Program

--- src/com/gla/arrays/PCM_Marks_Grade_By_Using_2D_Array.java
+++ src/com/gla/arrays/PCM_Marks_Grade_By_Using_2D_Array.java
@@ -1,31 +1,26 @@
 import java.util.Scanner;
 
-public class PCM_Marks_Grade {
+public class PCM_Marks_Grade_By_Using_2D_Array {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number of students : ");
         int n = sc.nextInt();
-        double[] p = new double[n];
-        double[] c = new double[n];
-        double[] m = new double[n];
+        int[][] marks = new int[n][3];
         double[] percentage = new double[n];
         char[] grade = new char[n];
         for(int i=0; i<n; i++){
-            System.out.println("\nStudent "+(i+1));
-            System.out.print("Enter Physics marks : ");
-            p[i] = sc.nextDouble();
-            System.out.print("Enter Chemistry marks : ");
-            c[i] = sc.nextDouble();
-            System.out.print("Enter Maths marks : ");
-            m[i] = sc.nextDouble();
-            if(p[i]<=0 || c[i]<=0 || m[i]<=0){
-                System.out.println("Invalid marks Enter Positive values");
-                i--;
-                continue;
+            System.out.println("\nEnter marks of student "+(i+1));
+            for(int j = 0; j <3; j++){
+                while (true){
+                    marks[i][j] = sc.nextInt();
+                    if(marks[i][j]>=0) break;
+                    System.out.println("Marks cannot be negative Please enter positive marks");
+                }
             }
         }
         for(int i=0; i<n; i++){
-            percentage[i] = (p[i]+c[i]+m[i])/3.0;
+            int total = marks[i][0]+marks[i][1]+marks[i][2];
+            percentage[i] = total/3.0;
             if(percentage[i]>=80) grade[i] = 'A';
             else if(percentage[i]>=70) grade[i] = 'B';
             else if(percentage[i]>=60) grade[i] = 'C';
@@ -34,9 +29,10 @@ public class PCM_Marks_Grade {
             else grade[i] = 'R';
         }
         for(int i=0; i<n; i++){
-            System.out.println("Physics "+p[i]);
-            System.out.println("Chemistry : "+c[i]);
-            System.out.println("Maths : "+m[i]);
+            System.out.println("Student "+(i+1));
+            System.out.println("Physics "+marks[i][0]);
+            System.out.println("Chemistry : "+marks[i][1]);
+            System.out.println("Maths : "+marks[i][2]);
             System.out.println("Percentage : "+percentage[i]);
             System.out.println("Grade : "+grade[i]);
             System.out.println();
