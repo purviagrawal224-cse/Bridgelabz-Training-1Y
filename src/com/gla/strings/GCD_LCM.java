9e90d7e2e35d5bb803ef1d612cf36e16f9d7a2ed
Purvi Agrawal <purviagrawal224@gmail.com>
2026-02-11 00:30:40 +0530

    [Purvi agrawal] Add. GCD_LCM Program

---
+++ src/com/gla/strings/GCD_LCM.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class GCD_LCM {
+    static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("GCD: " + gcd(a, b));
+        System.out.println("LCM: " + lcm(a, b));
+        sc.close();
+    }
+}
