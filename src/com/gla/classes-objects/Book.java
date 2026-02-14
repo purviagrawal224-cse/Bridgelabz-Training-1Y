4e0136180f3f36da4dd58c9dbe7dbf1eae34e3d4
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-02-14 22:53:23 +0530

    [Purvi agrawal] Add. Book Program

--- src/com/gla/classes-objects/Book.java
+++ src/com/gla/classes-objects/Book.java
@@ -1,25 +1,33 @@
+import java.util.Scanner;
 public class Book {
-    private String title;
-    private String author;
-    private double price;
-    public Book() {
-        this("Unknown", "Unknown", 0.0);
+    String title;
+    String author;
+    double price;
+    Book() {
+        this.title = "Unknown";
+        this.author = "Unknown";
+        this.price = 0.0;
     }
-    public Book(String title, String author, double price) {
+    Book(String title, String author, double price) {
         this.title = title;
         this.author = author;
         this.price = price;
     }
-    public String getTitle() { return title; }
-    public String getAuthor() { return author; }
-    public double getPrice() { return price; }
-    public void display() {
+    void display() {
         System.out.println("Title: " + title + ", Author: " + author + ", Price: " + price);
     }
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter title of the book: ");
+        String t = sc.nextLine();
+        System.out.print("Enter author of the book: ");
+        String a = sc.nextLine();
+        System.out.print("Enter price of the book: ");
+        double p = sc.nextDouble();
+        Book userBook = new Book(t, a, p);
         Book defaultBook = new Book();
-        Book customBook = new Book("1984", "George Orwell", 299.99);
         defaultBook.display();
-        customBook.display();
+        userBook.display();
+        sc.close();
     }
 }
