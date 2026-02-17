da3123ee054b65af0a7e19b88a8085da078b7831
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-02-17 21:15:15 +0530

    [Purvi agrawal] Add. Book Program

--- src/com/gla/inheritance-polymorphism/Library_Books.java
+++ src/com/gla/inheritance-polymorphism/Library_Books.java
@@ -1,17 +1,17 @@
 import java.util.*;
-class Library_Books {
+class Book {
     String t;
     int py;
-    Library_Books(String t, int py) {
+    Book(String t, int py) {
         this.t = t;
         this.py = py;
     }
     void displayInfo() {
         System.out.println("Title: " + t);
-        System.out.println("Publication Year: " + py);
+        System.out.println("Publication Year : "+ py);
     }
 }
-class Author extends Library_Books {
+class Author extends Book {
     String n;
     String b;
     Author(String t, int py, String n, String b) {
@@ -21,21 +21,21 @@ class Author extends Library_Books {
     }
     void displayInfo() {
         super.displayInfo();
-        System.out.println("Author Name: " + n);
+        System.out.println("Author Name : "+ n);
         System.out.println("Bio: " + b);
     }
 }
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter book title: ");
+        System.out.print("Enter book title : ");
         String t = sc.nextLine();
-        System.out.print("Enter publication year: ");
+        System.out.print("Enter publication year : ");
         int py = sc.nextInt();
         sc.nextLine();
-        System.out.print("Enter author name: ");
+        System.out.print("Enter author name : ");
         String n = sc.nextLine();
-        System.out.print("Enter author bio: ");
+        System.out.print("Enter author bio : ");
         String b = sc.nextLine();
         Author ob = new Author(t, py, n, b);
         System.out.println();
