13a832938b75440d711336aee057559ebad3185e
Purvi Agrawal <purviagrawal224@gmail.com>
2026-03-01 23:43:02 +0530

    [Purvi agrawal] Add. MainApp  Program

---
+++ src/com/gla/classes-objects/UniversityProject/MainApp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import com.university.department.cse.Course;
+public class MainApp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter course name: ");
+        String n = sc.nextLine();
+        System.out.print("Enter course code: ");
+        int c = sc.nextInt();
+        Course obj = new Course(n, c);
+        System.out.println("\n--- Course Details ---");
+        obj.show();
+        sc.close();
+    }
+}
