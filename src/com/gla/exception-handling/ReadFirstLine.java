4d88b8d07964d1f46de2baf6cf371e2a4b9b8af7
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-03-23 22:32:56 +0530

    [Purvi agrawal] Add. ReadFirstLine Program

--- src/com/gla/exception-handling/ReadFirstLine.java
+++ src/com/gla/exception-handling/ReadFirstLine.java
@@ -2,7 +2,7 @@ import java.io.*;
 public class ReadFirstLine {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("info.txt"))) {
-            String s = br.readLine();
+            String s=br.readLine();
             System.out.println(s);
         }
         catch(IOException e){
