7166fa069d9a792f043687d83fa9cf8a0139e8c9
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-04-11 14:16:54 +0530

    [Purvi agrawal] Add. FindSubsets Program

--- src/com/gla/generics-collections/FindSubsets.java
+++ src/com/gla/generics-collections/FindSubsets.java
@@ -1,11 +1,13 @@
 import java.util.*;
-public class FindSubsets {
-    static <T> boolean isSubset(Set<T> s1, Set<T> s2) {
-        return s2.containsAll(s1);
-    }
+public class FindSubset {
     public static void main(String[] args) {
-        Set<Integer> s1 = new HashSet<>(Arrays.asList(2, 3));
-        Set<Integer> s2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
-        System.out.println(isSubset(s1, s2));
+        Scanner sc = new Scanner(System.in);
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        int n1 = sc.nextInt();
+        for (int i = 0; i < n1; i++) s1.add(sc.nextInt());
+        int n2 = sc.nextInt();
+        for (int i = 0; i < n2; i++) s2.add(sc.nextInt());
+        System.out.println(s2.containsAll(s1));
     }
 }
