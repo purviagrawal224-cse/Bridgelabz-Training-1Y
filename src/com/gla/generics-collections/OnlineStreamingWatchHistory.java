eecdb019c097b34e2769f5b686ad897e705894dc
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-04-11 13:31:57 +0530

    [Purvi agrawal] Add. OnlineStreamingWatchHistory Program

--- src/com/gla/generics-collections/OnlineStreamingWatchHistory.java
+++ src/com/gla/generics-collections/OnlineStreamingWatchHistory.java
@@ -12,31 +12,38 @@ class Movie {
 }
 public class OnlineStreamingWatchHistory {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         List<Movie> all = new ArrayList<>();
-        all.add(new Movie("Movie1", "Action"));
-        all.add(new Movie("Movie2", "Comedy"));
-        all.add(new Movie("Movie3", "Action"));
-        all.add(new Movie("Movie4", "Drama"));
+        System.out.print("Enter number of movies: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String name = sc.nextLine();
+            String genre = sc.nextLine();
+            all.add(new Movie(name, genre));
+        }
         Queue<Movie> upNext = new LinkedList<>();
+        System.out.print("Enter number of movies to add in Up Next: ");
+        int m = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < m; i++) {
+            String name = sc.nextLine();
+            for (Movie mv : all) {
+                if (mv.name.equals(name)) upNext.add(mv);
+            }
+        }
         Stack<Movie> history = new Stack<>();
         Set<String> genres = new HashSet<>();
-        upNext.add(all.get(0));
-        upNext.add(all.get(1));
-        upNext.add(all.get(2));
         System.out.println("Watching Movies:");
         while (!upNext.isEmpty()) {
-            Movie m = upNext.remove();
-            System.out.println("Watched: " + m);
-            history.push(m);
-            genres.add(m.genre);
+            Movie mv = upNext.remove();
+            System.out.println("Watched: " + mv);
+            history.push(mv);
+            genres.add(mv.genre);
         }
-        System.out.println("Watch History:");
-        for (Movie m : history) System.out.println(m);
-        System.out.println("Genres Watched:");
-        for (String g : genres) System.out.println(g);
         System.out.println("Recommended Movies:");
-        for (Movie m : all) {
-            if (genres.contains(m.genre) && !history.contains(m)) System.out.println(m);
+        for (Movie mv : all) {
+            if (genres.contains(mv.genre) && !history.contains(mv)) System.out.println(mv);
         }
     }
 }
