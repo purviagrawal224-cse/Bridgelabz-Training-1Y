a2ecf45bb5f83bc7c1b9e62875b84bc3cd433849
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-04-11 14:27:25 +0530

    [Purvi agrawal] Add. RemoveDuplicates Program

--- src/com/gla/generics-collections/RemoveDuplicates.java
+++ src/com/gla/generics-collections/RemoveDuplicates.java
@@ -1,11 +1,12 @@
 import java.util.*;
-public class RemoveDuplicates {
-    static <T> List<T> removeDup(List<T> l) {
-        Set<T> s = new LinkedHashSet<>(l);
-        return new ArrayList<>(s);
-    }
+public class RemoveDuplicatesPreserveOrder {
     public static void main(String[] args) {
-        List<Integer> l = Arrays.asList(3, 1, 2, 2, 3, 4);
-        System.out.println(removeDup(l));
+        Scanner sc = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) list.add(sc.nextInt());
+        Set<Integer> set = new LinkedHashSet<>(list);
+        list = new ArrayList<>(set);
+        System.out.println(list);
     }
 }
