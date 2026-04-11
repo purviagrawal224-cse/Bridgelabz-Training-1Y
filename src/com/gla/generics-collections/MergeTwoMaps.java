23ef981fdca42848f5b3783eeb814e8f783a4731
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-04-11 13:13:50 +0530

    [Purvi agrawal] Add. MergeTwoMaps Program

--- src/com/gla/generics-collections/MergeTwoMaps.java
+++ src/com/gla/generics-collections/MergeTwoMaps.java
@@ -1,12 +1,27 @@
 import java.util.*;
 public class MergeTwoMaps {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Map<String, Integer> m1 = new HashMap<>();
-        m1.put("A", 1);
-        m1.put("B", 2);
         Map<String, Integer> m2 = new HashMap<>();
-        m2.put("B", 3);
-        m2.put("C", 4);
+        System.out.print("Enter size of Map1: ");
+        int n1 = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n1; i++) {
+            String k = sc.nextLine();
+            int v = sc.nextInt();
+            sc.nextLine();
+            m1.put(k, v);
+        }
+        System.out.print("Enter size of Map2: ");
+        int n2 = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n2; i++) {
+            String k = sc.nextLine();
+            int v = sc.nextInt();
+            sc.nextLine();
+            m2.put(k, v);
+        }
         for (Map.Entry<String, Integer> e : m2.entrySet()) {
             String k = e.getKey();
             int v = e.getValue();
