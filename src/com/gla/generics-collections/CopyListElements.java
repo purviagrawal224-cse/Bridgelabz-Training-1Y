bbf6685a794121913e262e94d040edc21ee71957
Purvi Agrawal <purviagrawal224@gmail.com>
2026-04-11 10:57:54 +0530

    [Purvi agrawal] Add. CopyListElements Program

---
+++ src/com/gla/generics-collections/CopyListElements.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class CopyListElements {
+    static void copyList(List<? super Number> dest, List<? extends Number> src) {
+        for (Number n : src) {
+            dest.add(n);
+        }
+    }
+    public static void main(String[] args) {
+        List<Integer> src = Arrays.asList(1, 2, 3);
+        List<Number> dest = new ArrayList<>();
+        copyList(dest, src);
+        System.out.println(dest);
+    }
+}
