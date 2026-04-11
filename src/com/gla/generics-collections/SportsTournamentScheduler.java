962e237295983e32fbc721e7406cf16311b478ab
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-04-11 13:26:34 +0530

    [Purvi agrawal] Add. SportsTournamentScheduler Program

--- src/com/gla/generics-collections/SportsTournamentScheduler.java
+++ src/com/gla/generics-collections/SportsTournamentScheduler.java
@@ -1,52 +1,40 @@
 import java.util.*;
-
 class Team implements Comparable<Team> {
     String name;
     int pts;
-
     Team(String name) {
         this.name = name;
         this.pts = 0;
     }
-
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Team)) return false;
         Team t = (Team) o;
         return name.equals(t.name);
     }
-
     public int hashCode() {
         return Objects.hash(name);
     }
-
     public int compareTo(Team o) {
-        if (o.pts != this.pts)
-            return o.pts - this.pts;
+        if (o.pts != this.pts) return o.pts - this.pts;
         return this.name.compareTo(o.name);
     }
-
     public String toString() {
         return name + " " + pts;
     }
 }
-
 class Match {
     Team t1, t2;
-
     Match(Team t1, Team t2) {
         this.t1 = t1;
         this.t2 = t2;
     }
 }
-
 class Result {
     String win;
-
     Result(String win) {
         this.win = win;
     }
-
     public String toString() {
         return "Winner: " + win;
     }
