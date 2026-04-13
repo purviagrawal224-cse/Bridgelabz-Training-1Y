28d651910934be27e4c091e948ebd7b25112d352
Purvi Agrawal <purviagrawal224@gmail.com>
2026-04-13 17:38:43 +0530

    [Purvi agrawal] Add. start Program

---
+++ src/com/gla/hello/hello.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+for(int i = 1; i<=n ;i++){
+if (i % 3 == 0 && i % 5 == 0){
+        
+              System.out.println(Answer[i] == "Fizz Buzz");
+           
+        }
+}
+        else if (i % 3 == 0) {
+            System.out.println(Answer[i] == "Fizz");
+        } 
+        else if (i % 5 == 0){
+            System.out.println(Answer[i] == "Buzz");
+        } 
+        else{
+               System.out.println(Divisible by none);
+    
+        }
+		}
+    }
+}
