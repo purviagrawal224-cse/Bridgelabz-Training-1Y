98bee270058b351bbd2b55524ff453872abd3a93
Purvi Agrawal <purviagrawal224@gmail.com>
2026-04-15 19:56:35 +0530

    [Purvi agrawal] Add. ExtractLinks Program

---
+++ src/com/gla/regular-expressions/ExtractLinks.java
@@ -0,0 +1,15 @@
+package Regex;
+import java.util.*;
+import java.util.regex.*;
+public class ExtractLinks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text:");
+        String s = sc.nextLine();
+        String r = "https?://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+        Pattern p = Pattern.compile(r);
+        Matcher m = p.matcher(s);
+        while (m.find()) System.out.println(m.group());
+        sc.close();
+    }
+}
