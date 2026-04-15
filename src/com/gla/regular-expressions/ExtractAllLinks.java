15965617a718413232bca4bd591bda4c63fb806a
Purvi Agrawal <purviagrawal224@gmail.com>
2026-04-15 20:55:04 +0530

    [Purvi agrawal] Add. ExtractAllLinks Program

---
+++ src/com/gla/regular-expressions/ExtractAllLinks.java
@@ -0,0 +1,20 @@
+package Regex;
+import java.util.*;
+import java.util.regex.*;
+public class ExtractAllLinks {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text:");
+        String s = sc.nextLine();
+        String r = "https?://[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}";
+        Pattern p = Pattern.compile(r);
+        Matcher m = p.matcher(s);
+        List<String> l = new ArrayList<>();
+        while (m.find()) l.add(m.group());
+        for (int i = 0; i < l.size(); i++) {
+            System.out.print(l.get(i));
+            if (i != l.size() - 1) System.out.print(", ");
+        }
+        sc.close();
+    }
+}
