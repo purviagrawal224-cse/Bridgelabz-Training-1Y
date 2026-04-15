d250d055f9b7222538e59ef0fa05a1d8bd539111
purviagrawal224-cse <purviagrawal224@gmail.com>
2026-04-15 20:38:22 +0530

    [Purvi agrawal] Add. CreditCardValidation Program

--- src/com/gla/regular-expressions/CreditCardValidation.java
+++ src/com/gla/regular-expressions/CreditCardValidation.java
@@ -4,7 +4,7 @@ import java.util.regex.*;
 public class CreditCardValidation {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter card number:");
+        System.out.println("Enter card number: ");
         String s = sc.nextLine();
         String r = "^(4\\d{15}|5\\d{15})$";
         if (Pattern.matches(r, s)) System.out.println("Valid");
