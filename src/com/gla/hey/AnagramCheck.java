a74ff1c99ecae67b9b1ab35ae69343dcfe5e176f
Purvi Agrawal <purviagrawal224@gmail.com>
2026-04-24 13:52:37 +0530

    [Purvi Agrawal] Add. AnagramCheck Program

---
+++ src/com/gla/hey/AnagramCheck.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class AnagramCheck {
+    public static void main(String[] args) {
+        String s = "listen";
+        String t = "silent";
+
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+
+        Arrays.sort(a);
+        Arrays.sort(b);
+
+        if (Arrays.equals(a, b)) {
+            System.out.println("Anagram");
+        } else {
+            System.out.println("Not Anagram");
+        }
+    }
+}
